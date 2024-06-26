d42a6ec0e6b89c192ce5ff22e84ee8ab87060579
pablolopez247 <pablo.lopez02@intouchcx.com>
2024-06-26 09:47:42 -0600

    Creating first version of the rpa

---
+++ rpa.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+import os
+
+chrome_options = Options()
+download_dir = os.path.dirname(os.path.realpath(__file__))
+prefs = {'download.default_directory' : download_dir}
+chrome_options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
