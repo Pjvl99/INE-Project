226666e176a93d4a621e8651b855d72a8250fa6c
pablolopez247 <pablo.lopez02@intouchcx.com>
2024-06-27 21:52:08 -0600

    Adding ine url

--- rpa.py
+++ rpa.py
@@ -5,9 +5,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 import os
+import time
 
 chrome_options = Options()
 download_dir = os.path.dirname(os.path.realpath(__file__))
 prefs = {'download.default_directory' : download_dir}
 chrome_options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+driver.get("https://datos.ine.gob.gt")
