38a8ff74d8a6229546fc7ec0494b4d732ae87007
pablolopez247 <pablo.lopez02@intouchcx.com>
2024-06-27 22:32:13 -0600

    Updating url

--- rpa.py
+++ rpa.py
@@ -12,5 +12,5 @@ download_dir = os.path.dirname(os.path.realpath(__file__))
 prefs = {'download.default_directory' : download_dir}
 chrome_options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
-driver.get("https://datos.ine.gob.gt")
+driver.get("https://datos.ine.gob.gt/dataset")
 time.sleep(10)
