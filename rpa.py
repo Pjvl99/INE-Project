24bd66e7abd700c335f9f8d4a65539dfd9a9af2f
pablolopez247 <pablo.lopez02@intouchcx.com>
2024-06-28 22:32:35 -0600

    Updating RPA

--- rpa.py
+++ rpa.py
@@ -7,10 +7,31 @@ from selenium import webdriver
 import os
 import time
 
-chrome_options = Options()
-download_dir = os.path.dirname(os.path.realpath(__file__))
-prefs = {'download.default_directory' : download_dir}
-chrome_options.add_experimental_option('prefs', prefs)
-driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
-driver.get("https://datos.ine.gob.gt/dataset")
-time.sleep(10)
+
+
+class INE:
+
+    def __init__(self) -> None:
+        self.start_chromedriver()
+        self.get_headers()
+
+
+    def start_chromedriver(self):
+        chrome_options = Options()
+        download_dir = os.path.dirname(os.path.realpath(__file__))
+        prefs = {'download.default_directory' : download_dir}
+        chrome_options.add_experimental_option('prefs', prefs)
+        self.driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+        self.driver.get("https://datos.ine.gob.gt/dataset")
+    
+    def get_headers(self) -> None:
+        headers = WebDriverWait(self.driver, 60).until(lambda header: header.find_elements(By.CSS_SELECTOR, '.dataset-heading > a'))
+        print(len(headers))
+
+
+
+def main():
+    INE()
+
+main()
+
